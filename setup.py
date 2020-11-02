bb130121494f83c74bb6b023bb3c3f2b4d754384
abahcheli <abahchel@uwo.ca>
2020-11-02 10:48:20 -0500

    Updated setup

--- setup.py
+++ setup.py
@@ -1,9 +1,25 @@
-import setuptools
+from setuptools import setup, find_packages
+from io import open
+from os import path
+import pathlib
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
 
-setuptools.setup(
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+long_description = README
+
+# automatically captured required modules for install_requires in requirements.txt
+with open(path.join(HERE, 'requirements.txt'), encoding='utf-8') as f:
+    all_reqs = f.read().split('\n')
+
+install_requires = [x.strip() for x in all_reqs if ('git+' not in x) and (not x.startswith('#')) and (not x.startswith('-'))]
+dependency_links = [x.strip().replace('git+', '') for x in all_reqs \
+                    if 'git+' not in x]
+
+setup(
     name="samfilt", # Replace with your own username
     version="0.0.1",
     author="Alec Bahcheli",
@@ -12,11 +28,13 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/abahcheli/samfilt",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
+    install_requires = install_requires,
+    python_requires='>=3.0',
+    scripts=['samfilt/samfilt'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Academic Free License (AFL)",
         "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
+    ]
 )
